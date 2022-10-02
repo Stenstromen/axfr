b4361285ece85a6c1e356db013a322ca3ba838d6
Stenstromen <peppoji@gmail.com>
2022-10-02 15:00:21 +0200

    Attempt att infinite scroll

--- controllers/axfr.controller.js
+++ controllers/axfr.controller.js
@@ -89,10 +89,41 @@ function sendNuRows(req, res) {
   });
 }
 
+function sendNuRowsInf(req, res) {
+  const date = req.params.date;
+  const page = req.params.page;
+  let rows1 = page * 18;
+  let rows2;
+  let maxPage;
+  if (page === "0") {
+    rows2 = 18;
+  } else {
+    rows2 = rows1 + 18;
+    console.log(rows2)
+  }
+
+  const maxSql = `SELECT amount FROM dates WHERE date = ${date}`;
+  nuCon.query(maxSql, function (err, result) {
+    if (err) throw err;
+    if (result === undefined || result.length == 0) {
+      console.log(`(.NU) Date ${date} empty or does not exist`); return
+    } else {
+      maxPage = Math.floor(result[0].amount / 18) + 1;
+    }
+  });
+
+  const sql = `SELECT domain FROM domains JOIN dates ON domains.dategrp = dates.id WHERE date = ${date} ORDER BY domain ASC LIMIT ${rows2}`;
+  nuCon.query(sql, function (err, result) {
+    if (err) throw err;
+    res.json(result);
+  });
+}
+
 module.exports = {
   sendIndex,
   sendSeDates,
   sendSeRows,
   sendNuDates,
   sendNuRows,
+  sendNuRowsInf,
 };
--- routers/axfr.router.js
+++ routers/axfr.router.js
@@ -8,5 +8,6 @@ axfrRouter.get("/se", axfrController.sendSeDates)
 axfrRouter.get("/se/:date/:page", axfrController.sendSeRows);
 axfrRouter.get("/nu", axfrController.sendNuDates)
 axfrRouter.get("/nu/:date/:page", axfrController.sendNuRows);
+axfrRouter.get("/nu/inf/:date/:page", axfrController.sendNuRowsInf);
 
 module.exports = axfrRouter;
